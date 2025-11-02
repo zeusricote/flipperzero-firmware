7f6da500247e31ec8d99d02395bc060b48fbd4cb
zeusricote <92752554+zeusricote@users.noreply.github.com>
2025-11-02 12:19:27 +0100

    Change default radio stack to light firmware

--- fbt_options.py
+++ fbt_options.py
@@ -27,9 +27,9 @@ COPRO_CUBE_VERSION = "1.20.0"
 COPRO_CUBE_DIR = "lib/stm32wb_copro"
 
 # Default radio stack
-COPRO_STACK_BIN = "stm32wb5x_BLE_Stack_full_fw.bin"
+COPRO_STACK_BIN = "stm32wb5x_BLE_Stack_light_fw.bin"
 # Firmware also supports "ble_full", but it might not fit into debug builds
-COPRO_STACK_TYPE = "ble_full"
+COPRO_STACK_TYPE = "ble_light"
 
 # Leave 0 to let scripts automatically calculate it
 COPRO_STACK_ADDR = "0x0"
